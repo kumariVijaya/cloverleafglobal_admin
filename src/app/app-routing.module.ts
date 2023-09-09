f64416e221eb19a345ab54975f87b678044eb8dd
Adarsh-ShreeNandik <adarsh@shreenandik.in>
2023-09-09 10:28:32 +0530

    cloverleaf frontend template breaking down into component , singup,login, work order list page with intigration of api.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,100 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './default-layout/header/header.component';
+import { LoginComponent } from './view/pages/login/login.component';
+import { SignupComponent } from './view/pages/signup/signup.component';
+import { DashboardComponent } from './view/pages/dashboard/dashboard.component';
+import { FreelancerDashboardComponent } from './view/pages/freelancer/freelancer-dashboard/freelancer-dashboard.component';
+import { FreelancerRegistrationComponent } from './view/pages/freelancer/freelancer-registration/freelancer-registration.component';
+import { FreelancerAssignmentComponent } from './view/pages/freelancer/freelancer-assignment/freelancer-assignment.component';
+import { WorkOrderDashboardComponent } from './view/pages/work-order/work-order-dashboard/work-order-dashboard.component';
+import { WorkOrderStatusComponent } from './view/pages/work-order/work-order-status/work-order-status.component';
+import { UserManagementComponent } from './view/pages/user-management/user-management.component';
+import { WorkOrderManagementComponent } from './view/pages/work-order/work-order-management/work-order-management.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path:'',
+    component:LoginComponent,
+    data:{
+      title:'Header'
+    }
+  },
+  {
+    path:'login',
+    component:LoginComponent,
+    data:{
+      title:'Header'
+    }
+  },
+  {
+    path:'signup',
+    component:SignupComponent,
+    data:{
+      title:'Header'
+    }
+  },
+  {
+    path:'dashboard',
+    component:DashboardComponent,
+    data:{
+      title:'Header'
+    }
+  },
+  {
+    path:'freelancer-dashboard',
+    component:FreelancerDashboardComponent,
+    data:{
+      title:'Header'
+    }
+  },
+ 
+  {
+    path:'freelancer-registration',
+    component:FreelancerRegistrationComponent,
+    data:{
+      title:'Header'
+    }
+  },
+ 
+  {
+    path:'freelancer-assignment',
+    component:FreelancerAssignmentComponent,
+    data:{
+      title:'Header'
+    }
+  },
+  {
+    path:'workorder-dashboard',
+    component:WorkOrderDashboardComponent,
+    data:{
+      title:'Header'
+    }
+  },
+  {
+    path:'workorder-status',
+    component:WorkOrderStatusComponent,
+    data:{
+      title:'Header'
+    }
+  },
+  {
+    path:'workorder-management',
+    component:WorkOrderManagementComponent,
+    data:{
+      title:'Header'
+    }
+  },
+  {
+    path:'user-management',
+    component:UserManagementComponent,
+    data:{
+      title:'Header'
+    }
+  },
+ 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,58 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';import {trigger,state,style,animate,transition}from '@angular/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HeaderComponent } from './default-layout/header/header.component';
+import { FooterComponent } from './default-layout/footer/footer.component';
+import { LoginComponent } from './view/pages/login/login.component';
+import { SignupComponent } from './view/pages/signup/signup.component';
+import { DashboardComponent } from './view/pages/dashboard/dashboard.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'; // <== add the imports!
+import { HttpClientModule } from '@angular/common/http';
+import { ToastrModule, ToastrService, } from 'ngx-toastr';
+import { CommonModule } from '@angular/common';
+import { SidebarComponent } from './view/comman/sidebar/sidebar.component';
+import { FreelancerDashboardComponent } from './view/pages/freelancer/freelancer-dashboard/freelancer-dashboard.component';
+import { FreelancerRegistrationComponent } from './view/pages/freelancer/freelancer-registration/freelancer-registration.component';
+import { FreelancerAssignmentComponent } from './view/pages/freelancer/freelancer-assignment/freelancer-assignment.component';
+import { WorkOrderDashboardComponent } from './view/pages/work-order/work-order-dashboard/work-order-dashboard.component';
+import { WorkOrderStatusComponent } from './view/pages/work-order/work-order-status/work-order-status.component';
+import { UserManagementComponent } from './view/pages/user-management/user-management.component';
+import { WorkOrderManagementComponent } from './view/pages/work-order/work-order-management/work-order-management.component';
+ 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LoginComponent,
+    SignupComponent,
+    DashboardComponent,
+    SidebarComponent,
+    FreelancerDashboardComponent,
+    FreelancerRegistrationComponent,
+    FreelancerAssignmentComponent,
+    WorkOrderDashboardComponent,
+    WorkOrderStatusComponent,
+    UserManagementComponent,
+    WorkOrderManagementComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    CommonModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot({preventDuplicates : true}),
+    AppRoutingModule,
+    FormsModule,                               
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [
+    ToastrService,
+    
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
